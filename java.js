22057bb39f0b0b9e774fb566552011c283eb1eaa
salinassalinaskevindaniel1-png <salinassalinaskevindaniel1@gmail.com>
2026-04-26 23:27:39 -0400

    java.js

---
+++ java.js
@@ -0,0 +1,9 @@
+// Función para cumplir con el requerimiento de Alertas 
+function mostrarAlerta() {
+    alert("Nueva noticia publicada: Revisa las últimas tendencias en software 2026");
+}
+
+// Pequeño mensaje de bienvenida al cargar
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Sistema de Noticias Cargado correctamente...");
+});
